eca1f7a8fd73d008a0d3790ce4908104e7a7bb60
HimanshuGit3936 <himanshushekhar3936@gmail.com>
2025-10-23 20:18:57 +0530

    This is first commit...

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry="Yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("eat something..... ");
+			
+		}
+		else
+		{
+			System.out.println("eat something..... ");	
+		}
+
+	}
+
+}
