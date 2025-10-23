2754c6685047e558b4fb386d8f2424ce98415439
HimanshuGit3936 <himanshushekhar3936@gmail.com>
2025-10-23 21:43:23 +0530

    commiting test2 to branch A

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry = "Yes";
+
+		if (hungry == "yes") {
+			System.out.println("eat something..... ");
+
+		} else {
+			System.out.println("eat something..... ");
+		}
+
+	}
+
+}
